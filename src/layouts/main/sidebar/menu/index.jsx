43b4f87b2d42063fd702ecba9c6d834fadf3583f
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-09-18 22:13:23 +0300

    feat: menu const eklendi.

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -1,240 +1,39 @@
 import { NavLink } from "react-router-dom";
 import classNames from "classnames";
 import { data } from "autoprefixer";
+import { mainMenu } from "../../../../utils/const";
+
 export default function Menu() {
   const links = [
     { id: 1, name: "Anasayfa", activeSvg: "", NotactiveSvg: "", url: "/" },
   ];
   return (
     <nav className="mt-0.5 mb-1">
-      <NavLink to="/" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M12 9c-2.209 0-4 1.791-4 4s1.791 4 4 4 4-1.791 4-4-1.791-4-4-4zm0 6c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm0-13.304L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM19 19.5c0 .276-.224.5-.5.5h-13c-.276 0-.5-.224-.5-.5V8.429l7-4.375 7 4.375V19.5z"
-                />
-              </svg>
-            )}{" "}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Anasayfa</div>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"
-                />
-              </svg>
-            )}{" "}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M10.25 4.25c-3.314 0-6 2.686-6 6s2.686 6 6 6c1.657 0 3.155-.67 4.243-1.757 1.087-1.088 1.757-2.586 1.757-4.243 0-3.314-2.686-6-6-6zm-9 6c0-4.971 4.029-9 9-9s9 4.029 9 9c0 1.943-.617 3.744-1.664 5.215l4.475 4.474-2.122 2.122-4.474-4.475c-1.471 1.047-3.272 1.664-5.215 1.664-4.971 0-9-4.029-9-9z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Keşfet</div>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M19.993 9.042C19.48 5.017 16.054 2 11.996 2s-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958zM12 20c-1.306 0-2.417-.835-2.829-2h5.658c-.412 1.165-1.523 2-2.829 2zm-6.866-4l.847-6.698C6.364 6.272 8.941 4 11.996 4s5.627 2.268 6.013 5.295L18.864 16H5.134z"
-                />
-              </svg>
-            )}{" "}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M11.996 2c-4.062 0-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958C19.48 5.017 16.054 2 11.996 2zM9.171 18h5.658c-.412 1.165-1.523 2-2.829 2s-2.417-.835-2.829-2z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Bildirimler</div>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M1.998 5.5c0-1.381 1.119-2.5 2.5-2.5h15c1.381 0 2.5 1.119 2.5 2.5v13c0 1.381-1.119 2.5-2.5 2.5h-15c-1.381 0-2.5-1.119-2.5-2.5v-13zm2.5-.5c-.276 0-.5.224-.5.5v2.764l8 3.638 8-3.636V5.5c0-.276-.224-.5-.5-.5h-15zm15.5 5.463l-8 3.636-8-3.638V18.5c0 .276.224.5.5.5h15c.276 0 .5-.224.5-.5v-8.037z"
-                />
-              </svg>
-            )}{" "}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M1.998 4.499c0-.828.671-1.499 1.5-1.499h17c.828 0 1.5.671 1.5 1.499v2.858l-10 4.545-10-4.547V4.499zm0 5.053V19.5c0 .828.671 1.5 1.5 1.5h17c.828 0 1.5-.672 1.5-1.5V9.554l-10 4.545-10-4.547z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Mesajlar</div>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M3 4.5C3 3.12 4.12 2 5.5 2h13C19.88 2 21 3.12 21 4.5v15c0 1.38-1.12 2.5-2.5 2.5h-13C4.12 22 3 20.88 3 19.5v-15zM5.5 4c-.28 0-.5.22-.5.5v15c0 .28.22.5.5.5h13c.28 0 .5-.22.5-.5v-15c0-.28-.22-.5-.5-.5h-13zM16 10H8V8h8v2zm-8 2h8v2H8v-2z"
-                />
-              </svg>
-            )}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M18.5 2h-13C4.12 2 3 3.12 3 4.5v15C3 20.88 4.12 22 5.5 22h13c1.38 0 2.5-1.12 2.5-2.5v-15C21 3.12 19.88 2 18.5 2zM16 14H8v-2h8v2zm0-4H8V8h8v2z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Listeler</div>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore" className="py-1 block group">
-        {({ isActive }) => (
-          <div
-            className={classNames(
-              "p-3 rounded-full inline-flex items-center transition-colors gap-5  group-hover:bg-[#eff3f41a]  ",
-              { "font-bold": isActive }
-            )}
-          >
-            {!isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z"
-                />
-              </svg>
-            )}
-            {isActive && (
-              <svg
-                viewBox="0 0 24 24 "
-                width={26.5}
-                height={26.5}
-                className="block"
-              >
-                <path
-                  fill="#fff"
-                  d="M10.25 4.25c-3.314 0-6 2.686-6 6s2.686 6 6 6c1.657 0 3.155-.67 4.243-1.757 1.087-1.088 1.757-2.586 1.757-4.243 0-3.314-2.686-6-6-6zm-9 6c0-4.971 4.029-9 9-9s9 4.029 9 9c0 1.943-.617 3.744-1.664 5.215l4.475 4.474-2.122 2.122-4.474-4.475c-1.471 1.047-3.272 1.664-5.215 1.664-4.971 0-9-4.029-9-9z"
-                />
-              </svg>
-            )}
-            <div className="pr-4 text-xl">Yer İşaretleri</div>
-          </div>
-        )}
-      </NavLink>
+      {mainMenu.map((menu, index) => (
+        <NavLink to={menu.path} className="py-[3px] block group">
+          {({ isActive }) => (
+            <div
+              className={classNames(
+                "p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]",
+                {
+                  "font-bold": isActive,
+                }
+              )}
+            >
+              <div className="w-[26.25px] h-[26.25px] relative">
+                {menu?.notification && (
+                  <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1.5 -right-1 flex items-center justify-center text-[11px]">
+                    {menu?.notification}
+                  </span>
+                )}
+                {!isActive && menu.icon.passive}
+                {isActive && menu.icon.active}
+              </div>
+              <div className="pr-4 text-xl">{menu.title}</div>
+            </div>
+          )}
+        </NavLink>
+      ))}
     </nav>
   );
 }
